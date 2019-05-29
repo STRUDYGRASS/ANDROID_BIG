d648456b20ec6420022e9679491a25d5a383f8ab
STRUDYGRASS <yifei-zhou@qq.com>
2019-05-29 14:02:01 +0800

    initial with ignore

---
+++ app/src/main/java/com/example/andorid_big/View/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.andorid_big.View;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.andorid_big.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
