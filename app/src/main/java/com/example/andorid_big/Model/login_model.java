346e7a3382242af137b305a33bff5c7fa3f87770
STRUDYGRASS <yifei-zhou@qq.com>
2019-06-01 15:03:35 +0800

    增加Lombok，写完Face网络方面代码，未作数据解析

--- app/src/main/java/com/example/andorid_big/Model/login_model.java
+++ app/src/main/java/com/example/andorid_big/Model/login_model.java
@@ -1,19 +1,22 @@
 package com.example.andorid_big.Model;
 
 import com.example.andorid_big.Contract.login_contract;
+import com.example.andorid_big.Model.FaceBaiduApi.AuthService;
 
 public class login_model implements login_contract.login_ModelInterface{
-    public login_model() {//初始化数据库和人脸识别服务
+    private String AccessToken;
 
+    public login_model() {//初始化数据库和人脸识别服务
+        AccessToken = AuthService.getAuth(); //每次启动获取一次token
     }
 
     @Override
     public void CheckOut_Count(String name,String account,final Login_Return login_return){
-        if (true/*数据库中有对应值*/){
-            login_return.BackWith_NameRepeat();
-        }
-        else{ //进入人脸识别界面
-
-        }
+//        if (true/*数据库中有对应值*/){
+//            login_return.BackWith_NameRepeat();
+//        }
+//        else{ //进入人脸识别界面
+//
+//        }
             }
 }
---
+++ app/src/main/java/com/example/andorid_big/bean/FaceAddLocation.java
@@ -0,0 +1,22 @@
+package com.example.andorid_big.bean;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class FaceAddLocation {
+    @Getter
+    @Setter
+    private double left;
+    @Getter
+    @Setter
+    private double top;
+    @Getter
+    @Setter
+    private double width;
+    @Getter
+    @Setter
+    private double height;
+    @Getter
+    @Setter
+    private int rotation;
+}
