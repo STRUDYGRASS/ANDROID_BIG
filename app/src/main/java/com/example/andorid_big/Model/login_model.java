71be43a0018fdf87a6382b0c37065b364b2e9e94
STRUDYGRASS <yifei-zhou@qq.com>
2019-06-05 00:55:13 +0800

    增加线程组逻辑，为日后打基础

--- app/src/main/java/com/example/andorid_big/Model/login_model.java
+++ app/src/main/java/com/example/andorid_big/Model/login_model.java
@@ -1,5 +1,8 @@
 package com.example.andorid_big.Model;
 
+import android.os.Handler;
+import android.os.HandlerThread;
+
 import com.example.andorid_big.Contract.login_contract;
 import com.example.andorid_big.Model.FaceBaiduApi.AuthService;
 import com.example.andorid_big.Model.FaceBaiduApi.FaceAdd;
@@ -7,8 +10,11 @@ import com.example.andorid_big.Model.FaceBaiduApi.FaceAdd;
 public class login_model implements login_contract.login_ModelInterface{
     private String AccessToken;
 
+    private Handler mHandler;
+    private boolean mRunning = false;
 
-    public login_model() {//初始化数据库和人脸识别服务
+
+    public login_model() {//初始化数据库和人脸识别服务以及线程
         new Thread() {
             @Override
             public void run(){
@@ -30,7 +36,8 @@ public class login_model implements login_contract.login_ModelInterface{
 //            public void run() {
 //                FaceAdd.add("2","ry",AccessToken);
 //            }
-//        }.start();
+//        }.start(); //接收完毕后可以在或者使用布尔变量改变后销毁线程
 
             }
+
 }
